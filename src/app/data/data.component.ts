04cf1e428ce4cfd85f16f608817d07c2402bc8c4
ziedth <zied.hack@gmail.com>
2019-08-28 23:32:55 +0100

    dataComponent

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataListService } from '../data-list.service';
+import { PagerComponent } from '../pager/pager.component';
 
 
 @Component({
   selector: 'app-data',
   template: `
-
+  <table>
+  <tr *ngFor="let person of personsData">
+  <td>{{person.name}}</td>
+  <td>{{person.surname}}</td>
+  </tr>
+  </table>
+  <pager [pageCount]="pageCount" (pageNumberClicked)="pageChanged($event)"></pager>
   `
 })
 export class DataComponent  {
-  constructor() { }
+  private personsData = null;
+  private pageCount: number;
+  constructor(private dataListService: DataListService) {
+  var response = this.dataListService.getData(1); //Request first page from the service
+  this.personsData = response.persons;
+  this.pageCount = response.totalCount / 10;//We will show 10 records per page.
+}
+pageChanged(pageNumber: number){
+var response = this.dataListService.getData(pageNumber); //Request data from the service with new page number
+
+this.personsData = response.persons;
 
 }
+}
+@NgModule({
+imports: [CommonModule],
+exports: [],
+declarations: [DataListComponent, PagerComponent],
+providers: [DataListService],
+})
+export class DataListModule { }
---
+++ src/app/products.ts
@@ -0,0 +1,16 @@
+export class products{
+name : string;
+price: string;
+description : string;
+}
+export const PROD = [
+  {name: 'Dr IQ'},
+  {name: 'Magneta'},
+  {name: 'Bombasto'},
+  {price: '122'},
+  {price: '120'},
+  {price: '150'},
+  {description: 'fdsfq'},
+  {description: 'fdfd'},
+  {description: 'vcxww'}
+];
