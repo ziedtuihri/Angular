e1f545eb385cefe825a2b9a8b049d66eb0299c8a
ziedth <zied.hack@gmail.com>
2019-09-04 20:40:14 +0100

    fix problem selected stack overflow

--- src/app/pager/pager.component.ts
+++ src/app/pager/pager.component.ts
@@ -1,10 +1,45 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
 selector: 'app-pager',
 template: `
-
+<div><select (change)="Selected($event)"><option *ngFor="let group of groups" [value]="group.name">{{group.name}}</option></select></div>
+<input type="button" (click)="getVal()" value="submit"/>
+<h4 [hidden]="hide">the name selected : {{ selected }} OR for click button : {{ button }} </h4>
 `
 })
 export class PagerComponent {
+
+selected : string = '';
+button : string = '';
+
+hide : boolean = true;
+  Selected(event: any){
+    //update the ui
+    this.selected = event.target.value;
+    this.hide=false;
+  }
+// or you can use the button :
+getVal(){
+this.button=this.selected;
+}
+groups=[
+  {
+    "name": "select your chois",
+    "items": ""
+  },
+     {
+       "name": "pencils",
+       "items": "red pencil"
+     },
+     {
+       "name": "rubbers",
+       "items": "big rubber"
+     },
+     {
+       "name": "rubbers1",
+       "items": "big rubber1"
+     }
+  ];
+
 
 }
